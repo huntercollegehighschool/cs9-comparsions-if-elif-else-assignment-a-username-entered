9b050eb4f6821a52a6be952fdfb2d114409644b9
Maximilian Song <94186708+a-username-entered@users.noreply.github.com>
2021-11-30 00:58:23 +0000

    Completed assignment

--- part4.py
+++ part4.py
@@ -25,3 +25,11 @@ opposite
 
 num1 = int(input('Enter a number: '))
 num2 = int(input('Enter another number: '))
+if num1 > 0 and num2 > 0:
+  print('positive')
+elif num1 < 0 and num2 < 0:
+  print('negative')
+elif num1 == 0 or num2 == 0:
+  print('zero')
+else:
+  print('opposite')
